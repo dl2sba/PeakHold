03a258e45fe99c6d38da6a96a335e737f35cb449
Dietmar, DL2SBA <pachube@web.de>
2020-03-19 18:48:53 +0100

    Add files via upload

---
+++ PeakHold/PeakHold.cpp
@@ -0,0 +1,135 @@
+//================================================================
+//
+//  tbd
+//
+//  (c) Dietmar Krause, DL2SBA 2017
+//
+//  License see https://creativecommons.org/licenses/by/4.0/
+
+#include <arduino.h>
+#include "PeakHold.h"
+
+
+MinOrMaxHold::MinOrMaxHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep) {
+  mPeakHoldTime = pPeakHoldTime;
+  mDecayTime = pDecayTime;
+  mDecayStep = pDecayStep;
+}
+
+MaximumHold::MaximumHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep) : MinOrMaxHold(pPeakHoldTime, pDecayTime, pDecayStep) {
+	mPeak = 0x0000;
+	mState = 0;
+}
+
+void MaximumHold::consume(uint16_t pVal) {
+	// new maximum value?
+	if ( pVal > mPeak ) {
+		//  yes
+		//  store new peak
+		mPeak = pVal;
+
+		// switch to peak hold phase
+		mState = 1;
+	} else if ( mState == 1 ) {
+		//  start peak hold counter
+		mCounter = mPeakHoldTime;
+		mState = 2;
+	} else if ( mState == 2 ) {
+		// in peak hold phase
+		// time elapsed?
+		if ( mCounter > 0 ) {
+			// no
+			// decrement
+			--mCounter;
+		} else {
+			//	yes
+			mState = 3;
+		}
+	} else if (mState == 3) {
+		//  in decay phase
+		mPeak -= mDecayStep;
+		mCounter = mDecayTime;
+		mState = 4;
+	} else if (mState == 4 ) {
+		// in decay phase
+		// time elapsed?
+		if ( mCounter > 0 ) {
+			//	no
+			// decrement
+			--mCounter;
+		} else {
+			//	yes
+			mState = 5;
+		}
+	} else if (mState == 5 ) {
+		// decay time elapsed
+		// at end of range?
+		if ( mPeak > mDecayStep ) {
+			// no
+			// decay more
+			mState = 3;
+		} else {
+			mState = 0;
+		}
+	}
+}
+
+MinimumHold::MinimumHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep) : MinOrMaxHold(pPeakHoldTime, pDecayTime, pDecayStep) {
+	mPeak = 0xffff;
+	mState = 0;
+}
+
+void MinimumHold::consume(uint16_t pVal) {
+	// new minimum value?
+	if ( pVal < mPeak ) {
+		//  yes
+		//  store new peak
+		mPeak = pVal;
+
+		// switch to peak hold phase
+		mState = 1;
+	} else if ( mState == 1 ) {
+		//  start peak hold counter
+		mCounter = mPeakHoldTime;
+		mState = 2;
+	} else if ( mState == 2 ) {
+		// in peak hold phase
+		// time elapsed?
+		if ( mCounter > 0 ) {
+			// no
+			// decrement
+			--mCounter;
+		} else {
+			//	yes
+			mState = 3;
+		}
+	} else if (mState == 3) {
+		//  in decay phase
+		mPeak += mDecayStep;
+		mCounter = mDecayTime;
+		mState = 4;
+	} else if (mState == 4 ) {
+		// in decay phase
+		// time elapsed?
+		if ( mCounter > 0 ) {
+			//	no
+			// decrement
+			--mCounter;
+		} else {
+			//	yes
+			mState = 5;
+		}
+	} else if (mState == 5 ) {
+		// decay time elapsed
+		// at end of range?
+		if ( mPeak < 0xffff - mDecayStep ) {
+			// no
+			// decay more
+			mState = 3;
+		} else {
+			mState = 0;
+		}
+	}
+}
+
+
---
+++ PeakHold/PeakHold.h
@@ -0,0 +1,57 @@
+//================================================================
+//
+//  tbd
+//
+//  (c) Dietmar Krause, DL2SBA 2017
+//
+//  License see https://creativecommons.org/licenses/by/4.0/
+
+#ifndef _PEAKHOLD_H
+#define _PEAKHOLD_H
+
+class MinOrMaxHold {
+  public:
+    MinOrMaxHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep);
+
+  public:
+    virtual void consume(uint16_t pVal);
+    
+	inline uint16_t getPeak(void) { return mPeak; }
+	
+	inline void setPeakHoldTime ( uint16_t x ) { mPeakHoldTime = x; }
+	inline void setDecayTime    ( uint16_t x ) { mDecayTime = x; }
+	inline void setDecayStep    ( uint16_t x ) { mDecayStep = x; }
+
+  protected:
+    uint8_t  mState;
+    uint16_t mCounter;
+    uint16_t mPeak;
+
+    uint16_t mPeakHoldTime;
+    uint16_t mDecayTime;
+    uint16_t mDecayStep;
+
+};
+
+
+class MaximumHold : public MinOrMaxHold {
+  public:
+    MaximumHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep);
+
+  public:
+    void consume(uint16_t pVal);
+
+};
+
+class MinimumHold : public MinOrMaxHold {
+  public:
+    MinimumHold(uint16_t pPeakHoldTime, uint16_t pDecayTime, uint16_t pDecayStep);
+
+  public:
+    void consume(uint16_t pVal);
+
+};
+
+
+#endif
+
